71ecc738adbaf4b4e6f43a7a0c7cf85aac864ced
IkobiLynch <82119066+IkobiLynch@users.noreply.github.com>
2023-03-12 23:32:26 -0500

    Progress?

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from . import db
+from werkzeug.security import generate_password_hash
 
-class Property(db.model):
-    __tablename__='properties'
+class Property(db.Model):
+    __tablename__='property'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
@@ -12,12 +13,30 @@ class Property(db.model):
     description = db.Column(db.String(500))
     photo = db.Column(db.String(100))
 
+    def __init__(self, title, bedroom_number, location, price, type, description, photo):
+        self.title = title
+        self.bedroom_number = bedroom_number
+        self.location = location
+        self.price =   price
+        self.type = type
+        self.description = description
+        self.photo = photo
+
     def photo_path(self):
         if self.photo:
             return f'/static/uploads/{self.photo}'
         else:
             return None
         
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
     def get_id(self):
         try:
             return #unicode(self, id) #python 2 support
